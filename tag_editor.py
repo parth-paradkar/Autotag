ca21d26f90d12e511f67c6e8ce454fa49766154b
Parth <parth.paradkar@iitkgp.ac.in>
2018-12-22 12:50:13 +0530

    Error occuring Not fixed

--- tag_editor.py
+++ tag_editor.py
@@ -1,11 +1,10 @@
 import os
-import eyed3 as eyed3
+import eyed3
 
 #The song name must be of the format "<artist> - <song name>.mp3"
 #create a directory that will store all the songs to be edited
-dir_path = '/home/parth/Music/tagging'
+dir_path = '/home/parth/Music/tagging/'
 os.chdir(dir_path)
-
 for song in os.listdir(dir_path):
     song_name, song_ext = os.path.splitext(song)
     song_artist, song_title = song_name.split(' - ')
